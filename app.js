3b4ab18683c5afb3535aea586fb423aeef161240
Wisbell <westleyisbell@gmail.com>
2017-06-09 11:13:04 -0500

    Added dotenv package to hide env variables

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const request = require('request')
 
 app.get('/api/drugs/:drugName', ({params: {drugName}}, res, next) => {
 
-  let url = ''
+  let url = `https://api.goodrx.com/compare-price?name=${drugName}&api_key=${process.env.API_KEY}&sig=${process.env.SIG}`
 
   let getData = () => {
     return new Promise( (resolve, reject) => {
